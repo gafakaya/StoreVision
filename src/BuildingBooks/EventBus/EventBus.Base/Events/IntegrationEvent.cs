d933c0353a5d1c8bc899406d0f32dac1cd4638a8
Mehmet Kaya <81460760+gafakaya@users.noreply.github.com>
2021-08-15 07:39:32 +0300

    EventBus Base

---
+++ src/BuildingBooks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Events
+{
+    public class IntegrationEvent
+    {
+        [JsonProperty]
+        public Guid Id { get; private set; }
+
+        [JsonProperty]
+        public DateTime CreateDate { get; private set; }
+
+        public IntegrationEvent()
+        {
+
+        }
+
+        [JsonConstructor]
+        public IntegrationEvent(Guid id
+            , DateTime createDate)
+        {
+            Id = id;
+            CreateDate = createDate;
+        }
+    }
+}
